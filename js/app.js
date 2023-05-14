22d37fd7301b1008e5d0219dd1edf300df3e9f4a
Misael Nicolás Pacheco Jiménez <68884962+ingnicolaspj@users.noreply.github.com>
2023-05-13 23:42:53 -0600

    ADD FILES LANDING CRECE V5

---
+++ js/app.js
@@ -0,0 +1,55 @@
+// Mostrar el botón cuando el usuario ha bajado 20 píxeles desde la parte superior de la página
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// Volver a la parte superior de la página cuando el usuario hace clic en el botón
+function topFunction() {
+  document.body.scrollTop = 0; // Para Safari
+  document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+}
+
+const links = document.getElementsByClassName('links')[0];
+const hamburger = document.getElementsByClassName("hamburger")[0];
+const menuHamburger = document.getElementById("hamburger");
+let abierto = false;
+
+const toggleMenu = ()=>{
+    links.classList.toggle('links2');
+    links.style.transition = "transform 0.5s ease-in-out";
+}
+
+hamburger.addEventListener('click', ()=>{
+    toggleMenu();
+    if(document.querySelector('.links.links2')){
+        abierto = true;
+    }else{
+        abierto = false;
+    }
+})
+
+window.addEventListener('click', (e) =>{
+    this.console.log(e.target);
+    if(abierto){
+        if (e.target !== menuHamburger) {
+          toggleMenu();
+          abierto = false;
+        }
+    }
+})
+
+window.addEventListener('resize', () =>{
+    if(screen.width > 800){
+        if(abierto){
+            toggleMenu();
+            links.style.transition = 'none';
+            abierto = false;
+        }
+    }
+})
